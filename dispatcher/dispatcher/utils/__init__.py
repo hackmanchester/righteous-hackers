95717bfddae2149a9fbd01bb8ee5f7fdcaf1ab0a
Dave Arter <dave@minus-zero.org>
2012-10-27 21:54:35 +0100

    Make sure the overdrive spaceframe is aligned with the output flange
    As above.

--- dispatcher/dispatcher/utils/__init__.py
+++ dispatcher/dispatcher/utils/__init__.py
@@ -1,6 +1,8 @@
 import glob
 import yaml
 import os
+import json
+from subprocess import Popen, PIPE
 from logging import getLogger
 
 from django.conf import settings
@@ -36,7 +38,41 @@ class TubeWrapper(object):
     def __init__(self, name):
         for k, v in load_config(name).items():
             setattr(self, k, v)
+        
+        if not os.path.isfile(self.script_path) and self.invocation_method == "stdin":
+            log.error("Couldn't find the tube script for %s", self.name)
     
     
-    def process(self, msg):
-        return msg
+    @property
+    def script_path(self):
+        """Return the absolute path to this tube's executable"""
+        return os.path.join(settings.TUBES_ROOT, self.name, "tube")
+    
+    @property
+    def is_async(self):
+        """
+        Returns true if this tube returns its result asynchronously, i.e. via Pusher
+        """
+        return self.result_method == "pusher"
+    
+    def process(self, message):
+        """Take a message and process it, returning the result if appropriate"""
+        if self.invocation_method == "stdin":
+            return self.invoke_stdin(message)
+        elif self.invocation_method == "pusher":
+            return self.invoke_pusher(message)
+        else:
+            log.warn("WTF is this tube's invocation method?!")
+    
+    
+    def invoke_stdin(self, message):
+        log.debug("Going to execute %s", self.script_path)
+        encoded = json.dumps(message)
+        proc = Popen([self.script_path], stdin=PIPE, stdout=PIPE, cwd=os.path.dirname(self.script_path))
+        result = proc.communicate(input=encoded)[0]
+        return result
+    
+    def invoke_pusher(self, message):
+        log.debug("Should use pusher to send a message to %s", self.name)
+        message['target'] = self.name
+        
--- dispatcher/dispatcher/utils/management/commands/read_input.py
+++ dispatcher/dispatcher/utils/management/commands/read_input.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 import random
+from hashlib import md5
 
 from django.core.management.base import BaseCommand, CommandError
 
@@ -11,11 +12,25 @@ log = getLogger(__name__)
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         print "Enter your message: "
-        msg = raw_input()
+        payload = raw_input()
+        
+        message = {
+            "id": md5(payload).hexdigest(),
+            "payload": payload
+        }
+        
         tubes = list_tubes()
-        tube = TubeWrapper(random.choice(tubes))
+        if len(args):
+            tube_name = args[0]
+        else:
+            tube_name = random.choice(tubes)
+        tube = TubeWrapper(tube_name)
         
         log.debug("Sending your message to tube '%s'" % tube.name)
         
-        log.debug("Result: %s" % tube.process(msg))
+        result = tube.process(message)
+        if tube.is_async:
+            log.debug("Sent message, tube is async and so will deliver result whenever..")
+        else:
+            log.debug("Result: %s" % result)
         
